b4cca7097ad5018a5ab6cb2982ed1d76e9e3596f
Zdeněk Kundrát <zdendaki@zdendaki.net>
2021-04-02 15:15:05 +0200

    bug fix

--- Z21lib/Messages/LocoModeMessage.cs
+++ Z21lib/Messages/LocoModeMessage.cs
@@ -8,6 +8,9 @@ namespace Z21lib.Messages
 {
     public class LocoModeMessage : Message
     {
+        public LocoModeMessage() : base(MessageType.LAN_GET_LOCOMODE)
+        {
 
+        }
     }
 }
